f74dbb6e9b89721f7839b2aee40ce6445267eca5
Kenny Do <kedo@patreon.com>
2018-04-13 13:19:05 -0700

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,24 @@ extern crate html5ever;
 extern crate hyper;
 extern crate hyper_tls;
 extern crate kuchiki;
-extern crate tokio_core;
 extern crate regex;
 extern crate rocket;
+extern crate tokio_core;
 
-use std::io;
-use std::default::Default;
-use futures::{future, stream, Future, Stream};
 use futures::future::Executor;
+use futures::{future, stream, Future, Stream};
 use html5ever::serialize::serialize;
-use tokio_core::reactor::Core;
 use hyper::Client;
 use kuchiki::traits::*;
 use kuchiki::NodeRef;
-use regex::{ Captures, Regex, RegexBuilder};
+use regex::{Captures, Regex, RegexBuilder};
+use std::default::Default;
+use std::io;
+use tokio_core::reactor::Core;
 mod colors;
 use rocket::response::content;
 
-
-
-
 use hyper::{Response, StatusCode};
-
 
 use tokio_core::reactor::Handle;
 
@@ -42,15 +38,10 @@ fn create_colors_regex() -> Regex {
 
     let joined_groups = &color_groupings.join("|")[..];
 
-    RegexBuilder::new(&[
-        "(?P<color>",
-        joined_groups,
-        ")"
-    ]
-    .concat()[..])
-    .case_insensitive(true)
-    .build()
-    .unwrap()
+    RegexBuilder::new(&["(?P<color>", joined_groups, ")"].concat()[..])
+        .case_insensitive(true)
+        .build()
+        .unwrap()
 }
 
 fn replace_color_names_in_text_child_nodes(node_ref: &NodeRef) {
@@ -59,14 +50,18 @@ fn replace_color_names_in_text_child_nodes(node_ref: &NodeRef) {
         child_node.as_text().and_then(|text_node| {
             let original_text = text_node.borrow().clone();
 
-            Some(text_node.replace(
-                colors_regex.replace_all(&original_text[..], "purple").to_string()
-            ))
+            Some(
+                text_node.replace(
+                    colors_regex
+                        .replace_all(&original_text[..], "purple")
+                        .to_string(),
+                ),
+            )
         });
     }
 }
 
-fn fetch_and_mutate_url(url: &String) -> String{
+fn fetch_and_mutate_url(url: &String) -> String {
     let mut core = Core::new().unwrap();
     let client = Client::new(&core.handle());
     let handle = &core.handle();
@@ -93,13 +88,13 @@ fn fetch_and_mutate_url(url: &String) -> String{
         .and_then(|html| {
             let document = kuchiki::parse_html().one(html);
             Ok(document)
-        }).and_then(|parsed_dom| {
+        })
+        .and_then(|parsed_dom| {
             for css_match in parsed_dom.select("li, b, a, p, td, div, span").unwrap() {
                 let as_node = css_match.as_node();
 
                 replace_color_names_in_text_child_nodes(as_node);
-            };
-
+            }
 
             let mut bytes = vec![];
             serialize(&mut bytes, &parsed_dom, Default::default()).unwrap();
@@ -111,7 +106,7 @@ fn fetch_and_mutate_url(url: &String) -> String{
 
 #[derive(FromForm)]
 struct Task {
-    url: String
+    url: String,
 }
 
 #[get("/purplize?<task>")]
